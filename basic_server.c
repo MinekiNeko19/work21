296764e216694d5cbcf60901a03f1a1ce940c45a
Sarah Leong <sleong20@stuy.edu>
2021-12-14 20:37:06 -0500

    loops and makes the input upper case as intended

--- basic_server.c
+++ basic_server.c
@@ -9,13 +9,20 @@ int main() {
 
   while (1) {
     from_client = server_handshake( &to_client );
-    printf("pid: %d\tfrom_client: %d\tto_client: %d\n", getpid(), from_client, to_client);
+    // printf("pid: %d\tfrom_client: %d\tto_client: %d\n", getpid(), from_client, to_client);
     
-    write(to_client,"from server",10);
-    printf("5. server sent response to client\n");
+    // get input from client
+    char input[100];
+    read(from_client, input, 100);
 
-    char final[10];
-    read(from_client, final, 10);
-    printf("7. final message from client: %s\n", final);
+    // modify input
+    int i = 0;
+    while (input[i] != '\n') {
+      input[i] = toupper(input[i]);
+      i++;
+    }
+
+    // sends input back to client
+    write(to_client, input, 100);
   }
 }
