15dea2699ce4b5c674a6db5e402b59df263a4a1e
Sarah Leong <sleong20@stuy.edu>
2021-12-14 20:25:15 -0500

    added the while loop; server is constantly on

--- basic_server.c
+++ basic_server.c
@@ -7,13 +7,15 @@ int main() {
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
-  printf("pid: %d\tfrom_client: %d\tto_client: %d\n", getpid(), from_client, to_client);
-  
-  write(to_client,"from server",10);
-  printf("5. server sent response to client\n");
+  while (1) {
+    from_client = server_handshake( &to_client );
+    printf("pid: %d\tfrom_client: %d\tto_client: %d\n", getpid(), from_client, to_client);
+    
+    write(to_client,"from server",10);
+    printf("5. server sent response to client\n");
 
-  char final[10];
-  read(from_client, final, 10);
-  printf("7. final message from client: %s\n", final);
+    char final[10];
+    read(from_client, final, 10);
+    printf("7. final message from client: %s\n", final);
+  }
 }
