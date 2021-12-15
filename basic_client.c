dc5b11334b09a15536ca0567cb5bc6a0f9e3461b
Sarah Leong <sleong20@stuy.edu>
2021-12-14 20:21:23 -0500

    successful in testing the pipings

--- basic_client.c
+++ basic_client.c
@@ -3,8 +3,20 @@
 
 int main() {
 
-  int to_server;
-  int from_server;
+    int to_server;
+    int from_server;
 
-  from_server = client_handshake( &to_server );
+    from_server = client_handshake( &to_server );
+    printf("pid: %d\tfrom_client: %d\tto_server: %d\n", getpid(), from_server, to_server);
+    
+    char sc[10];
+    read(from_server, sc, 10);
+    printf("6. client got from server: %s\n", sc);
+
+    char input[10];
+    printf("Enter input: ");
+    fgets(input, 10, stdin);
+    write(to_server, input, 10);
+    printf("2. client sends name of SP to server\n");
+
 }
--- basic_server.c
+++ basic_server.c
@@ -1,4 +1,5 @@
 #include "pipe_networking.h"
+#include <ctype.h>
 
 
 int main() {
@@ -7,4 +8,12 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+  printf("pid: %d\tfrom_client: %d\tto_client: %d\n", getpid(), from_client, to_client);
+  
+  write(to_client,"from server",10);
+  printf("5. server sent response to client\n");
+
+  char final[10];
+  read(from_client, final, 10);
+  printf("7. final message from client: %s\n", final);
 }
