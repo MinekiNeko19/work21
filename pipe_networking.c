1fa56034cf95a084199381ebd4f7761b357ff116
Sarah Leong <sleong20@stuy.edu>
2021-12-14 20:20:37 -0500

    changed the variables to work properly ll

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,23 +10,23 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  int from_client = 0;
+  // int from_client = 0;
 
   mkfifo(WKP,0644);
   printf("0. server creates WKP\n");
 
-  int ctos = open(WKP, O_RDONLY);
+  int from_client = open(WKP, O_RDONLY);
   char sp[sizeof(ACK)];
-  read(ctos, sp, sizeof(ACK));
+  read(from_client, sp, sizeof(ACK));
   printf("4. server recieves from client through WKP: %s\n", sp);
   remove(WKP);
 
-  int stoc = open(ACK, O_WRONLY);
-  write(stoc,"from server",10);
+  *to_client = open(ACK, O_WRONLY);
+  write(*to_client,"from server",10);
   printf("5. server sent response to client\n");
 
   char final[10];
-  read(ctos, final, 10);
+  read(from_client, final, 10);
   printf("7. final message from client: %s\n", final);
 
   return from_client;
@@ -42,23 +42,23 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int from_server = 0;
+  // int from_server = 0;
 
   mkfifo(ACK,0644);
   printf("1. client creates secret pipe\n");
 
-  int ctos = open(WKP, O_WRONLY);
-  write(ctos, ACK, sizeof(ACK));
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server, ACK, sizeof(ACK));
   printf("2. client sends name of SP to server\n");
 
-  int stoc = open(ACK, O_RDONLY);
+  int from_server = open(ACK, O_RDONLY);
   printf("3. client waiting for response\n");
   
   char sc[11];
-  read(stoc, sc, 11);
+  read(from_server, sc, 11);
   printf("6. client got from server: %s\n", sc);
   remove(ACK);
 
-  write(ctos, "from clie\n",10);
+  write(*to_server, "from clie\n",10);
     return from_server;
 }
